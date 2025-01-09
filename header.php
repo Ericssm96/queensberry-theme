63960e3d63430316058d78576f59cd951fe5283a
Eric Stéfano <ericssm96@gmail.com>
2025-01-08 22:46:27 -0300

    Product page styling finished

--- header.php
+++ header.php
@@ -1,13 +1,303 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-    <?php
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <?php
     $site_title = get_bloginfo('name');
-    ?>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
+  ?>
   <title><?= is_front_page() ? $site_title : strtoupper(get_the_title()) .  " - " . $site_title ?></title>
+  <link rel="stylesheet" href="./src/css/main_navigation.css" />
+  <?php wp_head(); ?>
+  <link rel="stylesheet" href="./src/css/home-page.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:wght@400;500;700;800&family=Tenor+Sans&display=swap"
+    rel="stylesheet" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" /> -->
+  <link rel="stylesheet" href="./src/css/swiper-bundle.css" />
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </head>
-<body <?php body_class(["relative", "font-accord"]); ?>>
+
+<body>
+  <div class="video-overlay"></div>
+  <nav
+    x-bind:class="isNavSelected || isMouseOverNav || isWindowScrolledPastThreshold ? 'desktop-navigation white-nav' : 'desktop-navigation'"
+    x-data="{
+    isNavActive: false,
+    isMouseOverNav: false,
+    isNavSelected: false,
+    isWindowScrolledPastThreshold: false
+  }" x-on:mouseover="isMouseOverNav = true" x-on:mousemove="isMouseOverNav = true"
+    x-on:mouseout="isMouseOverNav = false"
+    x-init="window.addEventListener('scroll', () => isWindowScrolledPastThreshold = window.scrollY > 150)">
+    <div class="nav-a">
+      <div class="wrapper">
+        <form action="" class="search-field">
+          <input type="text" x-on:blur="isNavSelected = false" x-on:focus="isNavSelected = true"
+            placeholder="BUSCA" /><button type="submit" class="search-btn" x-on:focus="isNavSelected = true"
+            x-on:blur="isNavSelected = false">
+            <i class="fa-solid fa-magnifying-glass btn-ico"></i>
+          </button>
+        </form>
+        <h1 class="site-logo">
+          <a href="#"><img
+              x-bind:src="isNavSelected || isMouseOverNav || isWindowScrolledPastThreshold ? '<?= get_template_directory_uri(); ?>/src/img/logo-escuro.png' : '<?= get_template_directory_uri(); ?>/src/img/logo.png'"
+              alt="Queensberry Viagens" title="Queensberry Viagens" class="logo-img" /></a>
+        </h1>
+        <div class="upper-right">
+          <span x-show="!isWindowScrolledPastThreshold" class="green-highlight currency-field">
+            <strong class="bold">US$ 1 = R$5,19 | € 1 = R$5,57</strong>
+            <p class="data">
+              <span class="bold">Data:</span>06/03/2024 às 12:44
+            </p>
+          </span>
+          <a href="#" class="green-highlight agent-field">
+            <svg width="13" class="user-icon" height="14" viewBox="0 0 13 14" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M8.90764 8.09375C10.7865 8.09375 12.3354 9.64258 12.3354 11.5215V12.1562C12.3354 12.8418 11.7768 13.375 11.1166 13.375H2.17913C1.49358 13.375 0.960375 12.8418 0.960375 12.1562V11.5215C0.960375 9.64258 2.48381 8.09375 4.36272 8.09375C5.09905 8.09375 5.42913 8.5 6.64788 8.5C7.84123 8.5 8.17131 8.09375 8.90764 8.09375ZM11.1166 12.1562V11.5215C11.1166 10.3027 10.1264 9.3125 8.90764 9.3125C8.52678 9.3125 7.9428 9.71875 6.64788 9.71875C5.32756 9.71875 4.74358 9.3125 4.36272 9.3125C3.14397 9.3125 2.17913 10.3027 2.17913 11.5215V12.1562H11.1166ZM6.64788 7.6875C4.61663 7.6875 2.99163 6.0625 2.99163 4.03125C2.99163 2.02539 4.61663 0.375 6.64788 0.375C8.65373 0.375 10.3041 2.02539 10.3041 4.03125C10.3041 6.0625 8.65373 7.6875 6.64788 7.6875ZM6.64788 1.59375C5.30217 1.59375 4.21038 2.71094 4.21038 4.03125C4.21038 5.37695 5.30217 6.46875 6.64788 6.46875C7.96819 6.46875 9.08538 5.37695 9.08538 4.03125C9.08538 2.71094 7.96819 1.59375 6.64788 1.59375Z"
+                fill="white" />
+            </svg>
+            Área do Agente
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="nav-b">
+      <div class="wrapper">
+        <ul class="nav-links">
+          <li class="nav-link destinos-dt">
+            <a href="#"><span class="dot-ref">Destinos</span></a>
+            <div class="mega-box">
+              <div class="content-a">
+                <div class="row">
+                  <h3>
+                    <header>
+                      <h3><a href="#" class="title-link">África</a></h3>
+                    </header>
+                  </h3>
+                  <ul class="mega-links">
+                    <li><a href="#">África do Sul</a></li>
+                    <li><a href="#">Egito</a></li>
+                    <li><a href="#">Marrocos</a></li>
+                    <li><a href="#">Quênia</a></li>
+                    <li><a href="#">Tanzânia</a></li>
+                    <li><a href="#" class="plus">[+]</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>
+                    <h3><a href="#" class="title-link">Américas</a></h3>
+                  </header>
+                  <ul class="mega-links">
+                    <li><a href="#">Argentina</a></li>
+                    <li><a href="#">Brasil</a></li>
+                    <li><a href="#">Canadá</a></li>
+                    <li><a href="#">Chile</a></li>
+                    <li><a href="#">Estados Unidos</a></li>
+                    <li><a href="#">México</a></li>
+                    <li><a href="#">Peru</a></li>
+                    <li><a href="#" class="plus">[+]</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>
+                    <h3><a href="#" class="title-link">Ásia</a></h3>
+                  </header>
+                  <ul class="mega-links">
+                    <li><a href="#">China</a></li>
+                    <li><a href="#">Índia</a></li>
+                    <li><a href="#">Japão</a></li>
+                    <li><a href="#">Maldivas</a></li>
+                    <li><a href="#">Tailândia</a></li>
+                    <li><a href="#" class="plus">[+]</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>
+                    <h3><a href="#" class="title-link">Europa</a></h3>
+                  </header>
+                  <ul class="mega-links">
+                    <li><a href="#">Alemanha</a></li>
+                    <li><a href="#">Croácia</a></li>
+                    <li><a href="#">Espanha</a></li>
+                    <li><a href="#">Grécia</a></li>
+                    <li><a href="#">Itália</a></li>
+                    <li><a href="#">Portugal</a></li>
+                    <li><a href="#">Rússia</a></li>
+                    <li><a href="#">Turquia</a></li>
+                    <li><a href="#" class="plus">[+]</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>
+                    <h3><a href="#" class="title-link">Oceania</a></h3>
+                  </header>
+                  <ul class="mega-links">
+                    <li><a href="#">Austrália</a></li>
+                    <li><a href="#">Nova Zelândia</a></li>
+                    <li><a href="#" class="plus">[+]</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>
+                    <h3><a href="#" class="title-link">Oriente Médio</a></h3>
+                  </header>
+                  <ul class="mega-links">
+                    <li><a href="#">Emirados Árabes</a></li>
+                    <li><a href="#">Irã</a></li>
+                    <li><a href="#">Israel</a></li>
+                    <li><a href="#">Jordânia</a></li>
+                    <li><a href="#">Líbano</a></li>
+                    <li><a href="#" class="plus">[+]</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="nav-link">
+            <a href="#"><span class="dot-ref">Produtos</span></a>
+            <div class="mega-box">
+              <div class="content-b">
+                <ul class="left-col">
+                  <li><a href="#">Férias na neve</a></li>
+                  <li><a href="#">Viagens personalizadas</a></li>
+                  <li><a href="#">Tours Regulares</a></li>
+                  <li><a href="#">Brasil In</a></li>
+                  <li><a href="#">Cruzeiros</a></li>
+                </ul>
+                <div class="right-col">
+                  <header>
+                    <h3><a href="#">GBM - Grupos Brasileiros No Mundo</a></h3>
+                  </header>
+                  <ul class="cards-area">
+                    <li>
+                      <a href="#">
+                        <img src="<?= get_template_directory_uri(); ?>/src/img/gbm-4-conti.jpg" alt="Imagem Promocional Grupo Brasil no Mundo" />
+                      </a>
+                      <p>4 continentes 2024</p>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <img src="<?= get_template_directory_uri(); ?>/src/img/gbm-4-conti.jpg" alt="Imagem Promocional Grupo Brasil no Mundo" />
+                      </a>
+                      <p>4 continentes 2024</p>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <img src="<?= get_template_directory_uri(); ?>/src/img/gbm-4-conti.jpg" alt="Imagem Promocional Grupo Brasil no Mundo" />
+                      </a>
+                      <p>4 continentes 2024</p>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <img src="<?= get_template_directory_uri(); ?>/src/img/gbm-4-conti.jpg" alt="Imagem Promocional Grupo Brasil no Mundo" />
+                      </a>
+                      <p>4 continentes 2024</p>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <img src="<?= get_template_directory_uri(); ?>/src/img/gbm-4-conti.jpg" alt="Imagem Promocional Grupo Brasil no Mundo" />
+                      </a>
+                      <p>4 continentes 2024</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="nav-link"><a href="#">Portal do Agente</a></li>
+          <li class="nav-link"><a href="#">Blog</a></li>
+          <li class="nav-link"><a href="#">Viagens de Incentivo</a></li>
+          <li class="nav-link"><a href="#">Quem Somos</a></li>
+          <li class="nav-link"><a href="#">Fale Conosco</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <nav class="mobile-navigation" x-data="{
+    isMobileNavOpen: false,
+    isMobileSearchOpen: false,
+    isWindowScrolledPastThreshold: false
+  }" x-init="window.addEventListener('scroll', () => isWindowScrolledPastThreshold = window.scrollY > 150)">
+    <div
+      x-bind:class="isMobileNavOpen || isMobileSearchOpen || isWindowScrolledPastThreshold ? 'nav-c white-mobile-nav' : 'nav-c'">
+      <h1>
+        <a href="#"><img
+            x-bind:src="isMobileNavOpen || isMobileSearchOpen || isWindowScrolledPastThreshold ? '<?= get_template_directory_uri(); ?>src/img/logo-escuro.png' : '<?= get_template_directory_uri(); ?>/src/img/logo.png'"
+            alt="Home - Queensberry" title="Home - Queensberry" /></a>
+      </h1>
+
+      <div class="light-buttons">
+        <label class="search-btn" id="whiteSearchBtnMb" for="mobile-search-toggle"><i
+            class="fa-solid fa-magnifying-glass"></i></label>
+        <label class="menu-btn" for="mobile-menu-toggle"><i class="fa-solid fa-bars"></i></label>
+      </div>
+    </div>
+    <input type="checkbox" id="mobile-menu-toggle" class="mobile-menu-toggle"
+      x-on:change="isMobileNavOpen = !isMobileNavOpen" />
+    <div class="nav-d">
+      <div class="dark-buttons">
+        <label class="search-btn" id="blackSearchBtnMb" for="mobile-search-toggle"><i
+            class="fa-solid fa-magnifying-glass"></i></label>
+        <label class="menu-btn" for="mobile-menu-toggle"><i class="fa-solid fa-bars"></i></label>
+      </div>
+      <ul class="menu-items-mb">
+        <li>
+          <label class="parent-item" for="destinos-mb"><span>Destinos</span><span class="icon"><i
+                class="fa-solid fa-caret-down"></i></span></label>
+          <input type="checkbox" name="dropdown-mb" id="destinos-mb" />
+          <ul class="destinos-items-mb">
+            <li><a href="#">África</a></li>
+            <li><a href="#">Américas</a></li>
+            <li><a href="#">Ásia</a></li>
+            <li><a href="#">Europa</a></li>
+            <li><a href="#">Oceania</a></li>
+            <li><a href="#">Oriente Médio</a></li>
+          </ul>
+        </li>
+        <li>
+          <label class="parent-item" for="produtos-mb">Produtos<span class="icon"><i
+                class="fa-solid fa-caret-down"></i></span></label>
+          <input type="checkbox" name="dropdown-mb" id="produtos-mb" />
+          <ul class="produtos-items-mb">
+            <li><a href="#">Férias na neve</a></li>
+            <li><a href="#">GBM - Grupos Brasileiros no Mundo</a></li>
+            <li><a href="#">Viagens personalizadas</a></li>
+            <li><a href="#">Tours regulares</a></li>
+            <li><a href="#">Brasil in</a></li>
+            <li><a href="#">Cruzerios</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Q Travel</a></li>
+        <li><a href="#">Viagens de Incentivo</a></li>
+        <li><a href="#">Quem Somos</a></li>
+        <li><a href="#">Fale Conosco</a></li>
+      </ul>
+      <div class="bottom-wrapper">
+        <a href="#" class="green-highlight agent-field">
+          <svg width="13" class="user-icon" height="14" viewBox="0 0 13 14" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M8.90764 8.09375C10.7865 8.09375 12.3354 9.64258 12.3354 11.5215V12.1562C12.3354 12.8418 11.7768 13.375 11.1166 13.375H2.17913C1.49358 13.375 0.960375 12.8418 0.960375 12.1562V11.5215C0.960375 9.64258 2.48381 8.09375 4.36272 8.09375C5.09905 8.09375 5.42913 8.5 6.64788 8.5C7.84123 8.5 8.17131 8.09375 8.90764 8.09375ZM11.1166 12.1562V11.5215C11.1166 10.3027 10.1264 9.3125 8.90764 9.3125C8.52678 9.3125 7.9428 9.71875 6.64788 9.71875C5.32756 9.71875 4.74358 9.3125 4.36272 9.3125C3.14397 9.3125 2.17913 10.3027 2.17913 11.5215V12.1562H11.1166ZM6.64788 7.6875C4.61663 7.6875 2.99163 6.0625 2.99163 4.03125C2.99163 2.02539 4.61663 0.375 6.64788 0.375C8.65373 0.375 10.3041 2.02539 10.3041 4.03125C10.3041 6.0625 8.65373 7.6875 6.64788 7.6875ZM6.64788 1.59375C5.30217 1.59375 4.21038 2.71094 4.21038 4.03125C4.21038 5.37695 5.30217 6.46875 6.64788 6.46875C7.96819 6.46875 9.08538 5.37695 9.08538 4.03125C9.08538 2.71094 7.96819 1.59375 6.64788 1.59375Z"
+              fill="white" />
+          </svg>
+          Área do Agente
+        </a>
+      </div>
+    </div>
+    <input type="checkbox" id="mobile-search-toggle" x-on:change="isMobileSearchOpen = !isMobileSearchOpen" />
+    <form action="" class="mobile-global-search">
+      <div class="wrapper">
+        <input type="text" placeholder="Busca" x-on:click="isMobileSearchOpen = !isMobileSearchOpen"
+          name="GLOBAL_SEARCH" />
+        <button type="submit">OK</button>
+      </div>
+    </form>
+  </nav>
