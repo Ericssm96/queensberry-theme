e9fffc3bd78b3027174ae46d9af88b14f794baa0
Eric Stéfano <ericssm96@gmail.com>
2025-04-08 23:36:31 -0300

    cache file update

---
+++ cached-videos-urls.php
@@ -0,0 +1,90 @@
+<?php return array (
+  0 => 
+  array (
+    'VideoSequencia' => 1,
+    'Link' => '8iCEySxJlto',
+    'Status' => 'I',
+    'DataInclusao' => '2022-12-20T15:26:00',
+    'Descricao' => 'Live - Lançamento GBM 2024',
+  ),
+  1 => 
+  array (
+    'VideoSequencia' => 2,
+    'Link' => 'g_-Px1_1ASA',
+    'Status' => 'I',
+    'DataInclusao' => '2022-12-20T15:25:00',
+    'Descricao' => 'Lançamento GBM 2023/2024',
+  ),
+  2 => 
+  array (
+    'VideoSequencia' => 3,
+    'Link' => 'ndd-ujbmTH8',
+    'Status' => 'I',
+    'DataInclusao' => '2022-12-20T15:25:00',
+    'Descricao' => 'GBM 20 anos',
+  ),
+  3 => 
+  array (
+    'VideoSequencia' => 4,
+    'Link' => 'fkSrTQ3TE6Q',
+    'Status' => 'I',
+    'DataInclusao' => '2022-12-20T15:25:00',
+    'Descricao' => 'Live - Lançamento GBM Temporada 2022/2023',
+  ),
+  4 => 
+  array (
+    'VideoSequencia' => 5,
+    'Link' => 'y3jjxSnNObY',
+    'Status' => 'I',
+    'DataInclusao' => '2022-12-20T15:25:00',
+    'Descricao' => 'Live - Uniworld Cruises: S.S. Sphinx',
+  ),
+  5 => 
+  array (
+    'VideoSequencia' => 6,
+    'Link' => 'yge67JCvSxI',
+    'Status' => 'I',
+    'DataInclusao' => '2022-12-20T15:25:00',
+    'Descricao' => 'Live - Alemanha: da Baviera à Saxônia',
+  ),
+  6 => 
+  array (
+    'VideoSequencia' => 7,
+    'Link' => 'l2pEIa7Y82o',
+    'Status' => 'A',
+    'DataInclusao' => '2024-09-18T03:00:00',
+    'Descricao' => 'LIVE | TUNÍSIA, A JORNADA',
+  ),
+  7 => 
+  array (
+    'VideoSequencia' => 8,
+    'Link' => 'JBnoB_zEY5A',
+    'Status' => 'A',
+    'DataInclusao' => '2024-09-18T03:00:00',
+    'Descricao' => 'LIVE | Lançamento FÉRIAS NA NEVE',
+  ),
+  8 => 
+  array (
+    'VideoSequencia' => 9,
+    'Link' => 'yHrsLgJLwA4',
+    'Status' => 'A',
+    'DataInclusao' => '2024-09-18T03:00:00',
+    'Descricao' => 'LIVE | Lançamento GBM 2024/2025',
+  ),
+  9 => 
+  array (
+    'VideoSequencia' => 10,
+    'Link' => '17D-bk9Y7Y0',
+    'Status' => 'A',
+    'DataInclusao' => '2024-09-18T03:00:00',
+    'Descricao' => 'Lançamento BRASIL IN',
+  ),
+  10 => 
+  array (
+    'VideoSequencia' => 11,
+    'Link' => 'lGFMtFWI5p8',
+    'Status' => 'A',
+    'DataInclusao' => '2024-09-18T03:00:00',
+    'Descricao' => 'LIVE | Arábia Saudita - O Berço do Islã',
+  ),
+);
---
+++ cached-world-regions.php
@@ -0,0 +1,44 @@
+<?php return array (
+  0 => 
+  array (
+    'CodigoRegiao' => 'AF',
+    'NomeRegiao' => 'África                        ',
+    'Status' => 'A',
+    'Ordem' => 1,
+  ),
+  1 => 
+  array (
+    'CodigoRegiao' => 'AM',
+    'NomeRegiao' => 'Américas                      ',
+    'Status' => 'A',
+    'Ordem' => 2,
+  ),
+  2 => 
+  array (
+    'CodigoRegiao' => 'AS',
+    'NomeRegiao' => 'Ásia                          ',
+    'Status' => 'A',
+    'Ordem' => 3,
+  ),
+  3 => 
+  array (
+    'CodigoRegiao' => 'EU',
+    'NomeRegiao' => 'Europa                        ',
+    'Status' => 'A',
+    'Ordem' => 4,
+  ),
+  4 => 
+  array (
+    'CodigoRegiao' => 'OC',
+    'NomeRegiao' => 'Oceania                       ',
+    'Status' => 'A',
+    'Ordem' => 5,
+  ),
+  5 => 
+  array (
+    'CodigoRegiao' => 'OM',
+    'NomeRegiao' => 'Oriente Médio                 ',
+    'Status' => 'A',
+    'Ordem' => 6,
+  ),
+);
